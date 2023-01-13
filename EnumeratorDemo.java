3c96d708ec832fdf37aebff8ac3a5762bbb15aec
deshmukhritesh18 <99178372+deshmukhritesh18@users.noreply.github.com>
2023-01-13 18:06:56 +0530

    Add files via upload

---
+++ EnumeratorDemo.java
@@ -0,0 +1,24 @@
+package com.bnt.colection;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class EnumeratorDemo {
+
+	public static void main(String[] args) {
+		Vector v=new Vector();
+		for(int i=0;i<=10;i++) {
+			v.addElement(i);
+		}
+		
+		Enumeration e=v.elements();
+		while(e.hasMoreElements()) {
+			Integer I=(Integer)e.nextElement();
+			
+			if(I%2==0) {
+				System.out.println(I);
+			}
+		}
+		System.out.println(v);
+	}
+}
---
+++ ListStudy.java
@@ -0,0 +1,22 @@
+package com.bnt.colection;
+
+import java.util.ArrayList;
+
+public class ListStudy {
+
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		
+		al.add("A");
+		al.add(10);
+		al.add("A");
+		al.add(null);
+		System.out.println(al);
+		al.remove(2);
+		System.out.println("Äfter remove 2 "+ al);
+		al.add(2, "R");
+		System.out.println("2 nd index add R "+ al);
+		al.add("N");
+		System.out.println(al);
+	}
+}
